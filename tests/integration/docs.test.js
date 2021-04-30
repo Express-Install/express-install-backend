f9d6f0a123e36ba957771f9f9ef09c38f31b1838
Kyle Pham <mrquangkhai98@set.edu.vn>
2021-05-01 01:22:00 +0700

    fix jest test

--- tests/integration/docs.test.js
+++ tests/integration/docs.test.js
@@ -3,6 +3,10 @@ const httpStatus = require('http-status');
 const app = require('../../src/app');
 const config = require('../../src/config/config');
 
+afterAll(async () => {
+  await new Promise(resolve => setTimeout(() => resolve(), 500)); // avoid jest open handle error
+});
+
 describe('Auth routes', () => {
   describe('GET /v1/docs', () => {
     test('should return 404 when running in production', async () => {
